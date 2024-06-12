6651603dd7d2ea9b2c2bc583e1632e239a4e5e6e
chydeepak7 <deepakchy793@gmail.com>
2024-06-12 23:39:37 +0545

    logout implemented successfully

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const login = (email, password) => async (dispatch) => {
   try {
-    console.log("Attempting login...");
     dispatch({
       type: "USER_LOGIN_REQUEST",
     });
@@ -12,16 +11,21 @@ export const login = (email, password) => async (dispatch) => {
     const { data } = await axios.post(
       "/api/users/login/",
       {
-        'username':email,
-        'password':password,
+        username: email,
+        password: password,
       },
       config
     );
-    console.log("Login successful:", data);
+
     dispatch({ type: "USER_LOGIN_SUCCESS", payload: data });
     localStorage.setItem("userInfo", JSON.stringify(data));
   } catch (error) {
     console.error("Login failed:", error.message);
     dispatch({ type: "USER_LOGIN_FAIL", payload: error.message });
   }
+};
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({type:'USER_LOGOUT'})
 };
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,83 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from '../actions/userActions'
 function Header() {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const dispatch = useDispatch()
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
   return (
-    <nav className="navbar navbar-expand-md sticky-top navbar-shrink py-3" id="mainNav">
-        <div className="container"><a className="navbar-brand d-flex align-items-center" href="/"><span>InternProject</span></a>
-        <button data-bs-toggle="collapse" className="navbar-toggler" data-bs-target="#navcol-1">
-            <span className="visually-hidden">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
-            <div className="collapse navbar-collapse" id="navcol-1">
-                <ul className="navbar-nav mx-auto">
-                    <li className="nav-item"><Link className="nav-link active" to="index.html">Home</Link></li>
-                    <li className="nav-item"><a className="nav-link" href="services.html">Find Events</a></li>
-                    <li className="nav-item"><Link className="nav-link" to="/create">Create Events</Link></li>
-                    
-                </ul><a className="btn btn-primary shadow" role="button" href="signup.html">Sign in</a>
-                <a className="btn btn-primary shadow" role="button" href="signup.html">Sign up</a>
+    <nav
+      className="navbar navbar-expand-md sticky-top navbar-shrink py-3"
+      id="mainNav"
+    >
+      <div className="container">
+        <a className="navbar-brand d-flex align-items-center" href="/">
+          <span>InternProject</span>
+        </a>
+        <button
+          data-bs-toggle="collapse"
+          className="navbar-toggler"
+          data-bs-target="#navcol-1"
+        >
+          <span className="visually-hidden">Toggle navigation</span>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navcol-1">
+          <ul className="navbar-nav mx-auto">
+            <li className="nav-item">
+              <Link className="nav-link active" to="index.html">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="services.html">
+                Find Events
+              </a>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/create">
+                Create Events
+              </Link>
+            </li>
+          </ul>
+          {userInfo ? (
+            <div>{userInfo.username}
+            <Link
+                className="btn btn-primary shadow"
+                role="button"
+                to="/login"
+                style={{marginLeft:'20px'}}
+                onClick={logoutHandler}
+              >
+                Logout
+              </Link></div>
+          ) : (
+            <div>
+              <a
+                className="btn btn-primary shadow"
+                role="button"
+                href="signup.html"
+              >
+                Sign in
+              </a>
+              <a
+                className="btn btn-primary shadow"
+                role="button"
+                href="signup.html"
+                style={{marginLeft:'20px'}}
+              >
+                Sign up
+              </a>
             </div>
+          )}
         </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Header
+export default Header;
